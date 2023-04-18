0fe1a90ae5ddf21a7c8ff0e73fc47f9ff28185c5
aquibajanikh <123448104+aquibajanikh@users.noreply.github.com>
2023-04-18 13:21:18 +0530

    Add files via upload

---
+++ Java Basics - 18th April 2023/HelloWorld.java
@@ -0,0 +1,47 @@
+public class HelloWorld {
+    public static void main(String[] args) {
+        int age = 65;
+        String name = "John";
+        boolean isEmployed = true;
+        float increment = (int) 0.00;
+        float baseSalary = (float) 10000.00;
+
+        // String result = (age > 50) ? "something there" : "Something else here";
+
+        // Decision Making
+        // if (isEmployed == true) {
+        // if (age > 50) {
+        // System.out.println("You are already employed and above 50");
+        // } else {
+        // System.out.println("You are already employed and under 50");
+        // }
+        // } else {
+        // System.out.println("You can look for a job");
+        // }
+
+        // if (isEmployed == true) {
+        // if (age >= 60) {
+        // increment = (float) 0.00;
+        // } else if (age >= 50) {
+        // increment = (float) 10.00;
+        // } else if (age >= 40) {
+        // increment = (float) 15.00;
+        // } else {
+        // increment = (float) 20.00;
+        // }
+        // } else {
+        // System.out.println("You are not employed");
+        // }
+
+        // System.out.println("Your increment is : " + increment);
+
+        // switch ((int) increment) {
+        // case 0:
+        // System.out.println("Your salary is : " + (float) baseSalary);
+        // break;
+        // default:
+        // System.out.println("Your salary is : " + (float) (baseSalary + (increment *
+        // baseSalary / 100)));
+        // }
+    }
+}
